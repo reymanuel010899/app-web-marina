d4d18054c8d97b6f647f490c0b03c40f98c05ffd
reymanuel010899 <reymanuelferrera78@gmail.com>
2024-01-05 00:05:01 -0400

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## webside
+
+interfas
+
+#### License
+
+MIT
---
+++ app_web_marina/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "webside",
+			"type": "module",
+			"label": _("webside")
+		}
+	]
